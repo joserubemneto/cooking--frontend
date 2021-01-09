d9439548efbfe8190546db883c793a63997469be
Jose Rubem <joserubemn31@gmail.com>
2021-01-09 00:18:03 +0000

    feat(feature/basic-components): added issue components and global styles

---
+++ src/components/MainStyles.jsx
@@ -0,0 +1,43 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+const TopTitle = styled.h2`
+  font-size: 2rem;
+  color: ${(props) => props.color || '#000'};
+`
+
+const HeadTitle = styled.h3`
+  font-size: 1.625rem;
+  color: ${(props) => props.color || '#000'};
+`
+
+const SlideTitle = styled(TopTitle)`
+  color: ${(props) => props.color || '#000'};
+`
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: ${(props) => props.font};
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  p {
+    margin: 0;
+  }
+
+  ul, li {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+`
+
+export { TopTitle, HeadTitle, SlideTitle, GlobalStyle }
