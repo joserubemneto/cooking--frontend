cdbf3ff74a63e4c71df780a24ae3736d7d0e6b44
Jose Rubem <joserubemn31@gmail.com>
2021-01-10 22:10:24 +0000

    feat(header): created Bloob component

---
+++ src/components/Header/Bloob.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Flex, Box } from '@chakra-ui/react'
+import { ReactComponent as Bloob1 } from '../../assets/Vetor Header 3.svg'
+import { ReactComponent as Bloob2 } from '../../assets/Vetor Header 2.svg'
+
+export const Bloob = () => {
+  return (
+    <Flex width={"150px"} ml={"2rem"}>
+      <Bloob2/>
+      <Box width={"100px"} ml={"2.5rem"}>
+        <Bloob1/>
+      </Box>
+    </Flex>
+  )
+}
+
