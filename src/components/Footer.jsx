138ac8bb617c4bda36f011fcaa6f872ce3bd8ba3
Jose Rubem <joserubemn31@gmail.com>
2021-01-10 19:09:51 +0000

    feat(feature/footer): created Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,143 @@
+import React, { useContext } from 'react'
+import { Logo } from './MainStyles'
+import { Link } from 'react-router-dom'
+import { ThemeContext } from 'styled-components'
+import { Flex, Box, Text } from '@chakra-ui/react'
+import useMedia from '../hooks/useMedia'
+import { ReactComponent as Bloob } from '../assets/Footer1.svg'
+import { ReactComponent as Bloob2 } from '../assets/Footer2.svg'
+
+const Footer = () => {
+  const { fonts, colors } = useContext(ThemeContext)
+  const mobile = useMedia('(max-width: 55rem)')
+
+  const especialRecipesPages = [
+    {
+      title: "Receitas de Ano Novo",
+      page: "ano-novo"
+    },
+    {
+      title: "Receitas de Natal",
+      page: "natal"
+    },
+    {
+      title: "Receitas de São João",
+      page: "sao-joao"
+    },
+    {
+      title: "Receitas de Páscoa",
+      page: "pascoa"
+    }
+  ]
+
+  const aboutPages = [
+    {
+      title: "Quem Somos",
+      page: "quem-somos",
+    },
+    {
+      title: "Contato",
+      page: "contato",
+    },
+    {
+      title: "Guia",
+      page: "guia",
+    },
+    {
+      title: "Termos de uso e política de privacidade",
+      page: "termos",
+    },
+  ]
+
+  return (
+    <Box
+      boxShadow={`0 -5px 5px -5px #C3C3C3`}
+    >
+      {!mobile ? (
+
+      <Flex
+        flexDirection="column"
+        m={"1.625rem 4rem"}
+      >
+        <Flex
+          justifyContent="space-between"
+        >
+          <Box>
+            <Logo
+              color={colors.text}
+              open={false}
+              fontSize={"1.75rem"}
+              font={fonts.logo}
+              >
+                Cooking
+              </Logo>
+            <Box
+              mt={"0.75rem"}
+              color="#000"
+            >
+              Aprenda a cozinhar <Text display="inline" color={colors.subTitle}>de verdade</Text>.
+            </Box>
+            <Flex>
+              <Box width="10px">
+                <Bloob2/>
+              </Box>
+              <Box width="10px" ml="4rem">
+                <Bloob/>
+              </Box>
+            </Flex>
+          </Box>
+          <Flex
+            justifyContent="space-between"
+            ml={"2rem"}
+            mr={"4rem"}
+          >
+            <Box>
+              <Text color={colors.subTitle} fontWeight={"bold"}>Especiais</Text>
+              {especialRecipesPages.map((page) => (
+                <Box
+                  key={page.page}
+                  fontSize={"0.9rem"}
+                  pt={"1rem"}
+                  _hover={{ color: `{colors.subTitle}`, fontWeight: "bold" }}
+                >
+                  <Link to={`/${page.page}`}>{page.title}</Link>
+                </Box>
+              ))}
+            </Box>
+            <Box ml={"8rem"}>
+              <Text color={colors.subTitle} fontWeight={"bold"}>Sobre</Text>
+              {aboutPages.map((page) => (
+                <Box
+                  key={page.page}
+                  fontSize={"0.9rem"}
+                  pt={"1rem"}
+                  _hover={{ color: `{colors.subTitle}`, fontWeight: "bold" }}
+                >
+                  <Link to={`/${page.page}`}>{page.title}</Link>
+                </Box>
+              ))}
+            </Box>
+          </Flex>
+        </Flex>
+        <Box pt="10px">
+          <Text fontSize={"0.75rem"}>© Cooking™, 2021. All rights reserved.</Text>
+        </Box>
+      </Flex>
+      ) : (
+        <Flex alignItems="center" justifyContent="space-between" padding={"1rem"} borderRadius="4px" bgGradient={"linear(201.73deg, #EE6737 -7.35%, #F73C2C 103.6%)"}>
+          <Logo
+            color={colors.text}
+            open={true}
+            fontSize={"1.75rem"}
+            font={fonts.logo}
+          >
+            Cooking
+          </Logo>
+          <Text color="#FFF">© Cooking™, 2021. All rights reserved.</Text>
+        </Flex>
+      )}
+    </Box>
+  )
+}
+
+export default Footer
