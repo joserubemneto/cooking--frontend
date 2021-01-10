d6632bc1e5c27919e4fa09e2b5b09131a25b409f
Jose Rubem <joserubemn31@gmail.com>
2021-01-10 00:56:05 +0000

    feat(feature/header): added svg's

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom'
 import { ThemeContext } from 'styled-components'
 import { Head, Logo, Nav, NavLinks, StyledBurger } from './MainStyles'
 import useMedia from '../hooks/useMedia'
-import { InputGroup, Input, InputRightElement } from '@chakra-ui/react'
+import { InputGroup, Input, InputRightElement, Flex, Box} from '@chakra-ui/react'
+import { ReactComponent as Bloob } from '../assets/Vetor Header 3.svg'
+import { ReactComponent as Bloob2 } from '../assets/Vetor Header 2.svg'
 import { ReactComponent as Lupa } from '../assets/Lupa.svg'
+import bloob3 from '../assets/Vetor Header 1.svg'
 
 const Header = () => {
   const { fonts, colors } = useContext(ThemeContext)
@@ -13,12 +16,11 @@ const Header = () => {
   const large = useMedia('(min-width: 62.5rem)')
 
   return (
-    <Head mobile={mobile}>
+    <Head mobile={mobile} url={bloob3}>
       <Nav background={mobile}>
         <Link to="/">
-          <Logo color={colors.text} mobile={mobile} font={fonts.logo}>Cooking</Logo>
+          <Logo color={colors.text} mobile={mobile} open={burguerOpen} font={fonts.logo}>Cooking</Logo>
         </Link>
-
         <NavLinks color={colors.text} hover={colors.subTitle} open={burguerOpen}>
           <ul>
             <li>
@@ -42,7 +44,6 @@ const Header = () => {
             </div>
           )}
         </NavLinks>
-
         {mobile && (
           <StyledBurger onClick={() => setBurgerOpen(!burguerOpen)}>
             <div />
@@ -55,13 +56,13 @@ const Header = () => {
           <InputGroup
             alignItems="center"
             maxWidth={"300px"}
-            ml={"7.5rem"}
           >
             <Input
+              backgroundColor={"#FFF"}
               border={"none"}
               boxShadow={"0px 5px 5px rgba(75, 93, 104, 0.1)"}
               borderRadius={"50px"}
-              placeholder="O que você quer cozinhar hoje?"
+              placeholder="O que você quer cozinhar?"
               _focus={{ boxShadow: `0px 2px 4px 0px ${colors.subTitle}`, color: `${colors.text}` }}
             />
             <InputRightElement p={'.7rem'}>
@@ -71,6 +72,16 @@ const Header = () => {
         )}
 
       </Nav>
+
+      {!mobile && (
+        <Flex ml={"2.5rem"}>
+          <Bloob2/>
+          <Box ml={"2.5rem"}>
+            <Bloob/>
+          </Box>
+        </Flex>
+      )}
+
     </Head>
   )
 }
