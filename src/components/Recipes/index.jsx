053fdd5a3e64b75ed1f4f4a0fe9586e47d9edb29
Jose Rubem <joserubemn31@gmail.com>
2021-01-19 00:04:48 +0000

    refactor(recipes): code improvements

--- src/components/Recipes/index.jsx
+++ src/components/Recipes/index.jsx
@@ -1,156 +1,160 @@
 import React from 'react'
-import {TopTitle, HeadTitle} from '../MainStyles'
-import { Grid, GridItem, Image } from "@chakra-ui/react"
+import { TopTitle, HeadTitle } from '../MainStyles'
+import { Grid, GridItem, Image, Text, Flex } from "@chakra-ui/react"
 import theme from '../../theme'
-import {CategoryTitle} from './styles'
-import {Box} from '../CarouselOne/styles'
+import { SlideTitle } from '../MainStyles'
 import Elipse from '../../assets/Ellipse75.svg'
-import { Flex, Spacer } from "@chakra-ui/react"
-import {SlideTitle} from '../MainStyles'
 import Back from '../../assets/Image4.jpg'
 
 
 const RecipesComponent = () => {
+
   const categories = [
-    "Bolos e Tortas", "Camarões", "Carnes", "Empanados", "Microondas", "Petiscos", "Bebidas", "Bolos e Tortas", "Camarões", "Carnes", "Empanados", "Doces", "Veganos"
-  ];
-
-
-  function CreateCategories(props){
-    const myCategories = props.name.map((category, index) => {
-      if(index === 0){
-        return <CategoryTitle>{category}</CategoryTitle>
-      } else if (index % 2 == 0){
-        return (
-        <>
-       <img src={Elipse} alt=""/>
-       <CategoryTitle>{category}</CategoryTitle>
-       </>
-        )
-      } else if (index % 2 != 0){
-        return (
-        <>
-       <img src={Elipse} alt=""/>
-       <CategoryTitle weight="600">{category}</CategoryTitle>
-       </>
-        )
-      }
-    });
-
-    return myCategories;
-  }
+    {
+      name: "Bolos",
+      tag: "yes"
+    },
+    {
+      name: "Tortas",
+      tag: "yes"
+    },
+    {
+      name: "Carnes",
+      tag: "yes"
+    },
+    {
+      name: "Camarões",
+      tag: "yes"
+    },
+    {
+      name: "Empanados",
+      tag: "yes"
+    },
+    {
+      name: "Doces",
+      tag: "yes"
+    },
+    {
+      name: "Veganos",
+      tag: "no"
+    },
+    {
+      name: "Aves",
+      tag: "no"
+    },
+    {
+      name: "Hambúrguer",
+      tag: "no"
+    },
+    {
+      name: "Pizzas",
+      tag: "no"
+    },
+  ]
 
   return (
-    <>
-      <Grid
+    <Grid
       templateColumns="11% 78% 11%"
       templateRows="repeat(4, auto)"
-      >
-        <GridItem
+      mt="3rem"
+    >
+      <GridItem
         colStart={2}
         rowStart={1}
+      >
+        <TopTitle
+          weight="600"
+          color={theme.colors.title}
+          Talign='left'
         >
-          <TopTitle weight="600" color={theme.colors.title}Talign='left'>Categorias</TopTitle>
-        </GridItem>
-        <GridItem
+          Categorias
+        </TopTitle>
+      </GridItem>
+      <GridItem
         colStart={2}
         rowStart={2}
+        mt="0.5rem"
+      >
+        <Flex
+          justify="center"
+          align="center"
         >
-          <Flex>
-          <CreateCategories name={categories} />
-          </Flex>
-        </GridItem>
-        <GridItem
+          {categories.map((category, index) => (
+            <Flex
+              key={category.name}
+              align="center"
+              justify="space-between"
+            >
+              <Image
+                src={Elipse}
+                alt={"Elipse"}
+                p="0 1rem"
+                display={index === 0 && "none"}
+                visibility={index === 0 && "hidden"}
+              />
+              <Text
+                fontWeight={index % 2 !== 0 && "bold"}
+                textTransform="uppercase"
+                fontSize="1.2rem"
+              >
+                {category.name}
+              </Text>
+            </Flex>
+          ))}
+        </Flex>
+      </GridItem>
+      <GridItem
         colStart={2}
         rowStart={3}
-        mt="30px"
+        mt="2rem"
+      >
+        <HeadTitle
+          weight="600"
+          color={theme.colors.subTitle}
         >
-          <HeadTitle weight="600" color={theme.colors.subTitle}>Mais acessadas</HeadTitle>
-        </GridItem>
-        <GridItem
+          Mais acessadas
+        </HeadTitle>
+      </GridItem>
+      <GridItem
         colStart={2}
         rowStart={4}
-        mt="30px"
+        mt="1rem"
+      >
+        <Grid
+          templateColumns="1fr 1fr 1fr"
+          templateRows="auto auto"
+          gap="20px"
         >
-          <Grid
-            templateColumns="1fr 1fr 1fr"
-            templateRows="auto auto"
-            gap="20px"
-          >
-            <Box mobile={true}>
-            <SlideTitle color={theme.colors.slide}>{categories[0]}</SlideTitle>
-              <Image
-                boxSize="100%"
-                objectFit="cover"
-                src={Back}
-                alt="Dan Abramov"
-                position="absolute"
-                zIndex="-1"
+          {categories.map((category) => (
+            category.tag === "yes" && (
+              <Flex
+                w={"100%"}
+                h="290px"
+                position="relative"
+                bgColor="rgba(0, 0, 0, 0.5)"
+                justify="center"
+                align="center"
                 borderRadius="2rem"
-              />
-            </Box>
-            <Box mobile={true}>
-            <SlideTitle color={theme.colors.slide}>{categories[1]}</SlideTitle>
-              <Image
-                boxSize="100%"
-                objectFit="cover"
-                src={Back}
-                alt="Dan Abramov"
-                position="absolute"
-                zIndex="-1"
-                borderRadius="2rem"
-              />
-            </Box>
-            <Box mobile={true}>
-            <SlideTitle color={theme.colors.slide}>{categories[2]}</SlideTitle>
-              <Image
-                boxSize="100%"
-                objectFit="cover"
-                src={Back}
-                alt="Dan Abramov"
-                position="absolute"
-                zIndex="-1"
-                borderRadius="2rem"
-              />
-            </Box>
-            <Box mobile={true}>
-            <SlideTitle color={theme.colors.slide}>{categories[3]}</SlideTitle>
-              <Image
-                boxSize="100%"
-                objectFit="cover"
-                src={Back}
-                alt="Dan Abramov"
-                position="absolute"
-                zIndex="-1"
-                borderRadius="2rem"
-              />
-            </Box>
-            <Box mobile={true}>
-            <SlideTitle color={theme.colors.slide}>{categories[4]}</SlideTitle>
-              <Image
-                boxSize="100%"
-                objectFit="cover"
-                src={Back}
-                alt="Dan Abramov"
-                position="absolute"
-                zIndex="-1"
-                borderRadius="2rem"
-              />
-            </Box>
-            <Box mobile={true}><SlideTitle color={theme.colors.slide}>{categories[5]}</SlideTitle>
-              <Image
-                boxSize="100%"
-                objectFit="cover"
-                src={Back}
-                alt="Dan Abramov"
-                position="absolute"
-                zIndex="-1"
-                borderRadius="2rem"
-              /></Box>
-          </Grid>
-        </GridItem>
-      </Grid>
-    </>
+              >
+                <SlideTitle color={theme.colors.slide}>
+                  {category.name}
+                </SlideTitle>
+
+                <Image
+                  boxSize="100%"
+                  objectFit="cover"
+                  src={Back}
+                  alt="Dan Abramov"
+                  position="absolute"
+                  zIndex="-1"
+                  borderRadius="2rem"
+                />
+              </Flex>
+            )
+          ))}
+        </Grid>
+      </GridItem>
+    </Grid>
   )
 }
 
