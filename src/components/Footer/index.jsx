cb810ba81c8f5691a0216c358058885e99713439
Jose Rubem <joserubemn31@gmail.com>
2021-01-21 22:33:37 +0000

    refactor(routes): adjust routes name

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,129 +1,123 @@
-import React, { useContext } from 'react'
-import { Logo } from '../MainStyles'
-import { Link } from 'react-router-dom'
-import { ThemeContext } from 'styled-components'
-import { Flex, Box, Text } from '@chakra-ui/react'
-import useMedia from '../../hooks/useMedia'
-import { ReactComponent as Bloob } from '../../assets/Vetor Footer 2.svg'
-import { ReactComponent as Bloob2 } from '../../assets/Vetor Footer 1.svg'
+import React, { useContext } from "react"
+import { Logo } from "../MainStyles"
+import { Link } from "react-router-dom"
+import { ThemeContext } from "styled-components"
+import { Flex, Box, Text } from "@chakra-ui/react"
+import useMedia from "../../hooks/useMedia"
+import { ReactComponent as Bloob } from "../../assets/Vetor Footer 2.svg"
+import { ReactComponent as Bloob2 } from "../../assets/Vetor Footer 1.svg"
 
 const Footer = () => {
   const { fonts, colors } = useContext(ThemeContext)
-  const mobile = useMedia('(max-width: 55rem)')
+  const mobile = useMedia("(max-width: 55rem)")
 
   const especialRecipesPages = [
     {
       title: "Receitas de Ano Novo",
-      page: "ano-novo"
+      page: "ano-novo",
     },
     {
       title: "Receitas de Natal",
-      page: "natal"
+      page: "natal",
     },
     {
       title: "Receitas de São João",
-      page: "sao-joao"
+      page: "sao-joao",
     },
     {
       title: "Receitas de Páscoa",
-      page: "pascoa"
-    }
+      page: "pascoa",
+    },
   ]
 
   const aboutPages = [
     {
       title: "Quem Somos",
-      page: "about",
+      page: "sobre",
     },
     {
       title: "Contato",
-      page: "about",
+      page: "sobre",
     },
     {
       title: "Guia",
-      page: "about",
+      page: "sobre",
     },
     {
       title: "Termos de uso e política de privacidade",
-      page: "about",
+      page: "sobre",
     },
   ]
 
   return (
-    <Box
-      boxShadow={`0 -5px 5px -5px #C3C3C3`}
-      pt={"2.75rem"}
-      mt={"7rem"}
-    >
+    <Box boxShadow={`0 -5px 5px -5px #C3C3C3`} pt={"2.75rem"} mt={"7rem"}>
       {!mobile ? (
-      <Flex
-        flexDirection="column"
-        m={"1.625rem 4rem"}
-      >
-        <Flex
-          justifyContent="space-between"
-        >
-          <Box>
-            <Logo
-              color={colors.text}
-              open={false}
-              fontSize={"1.75rem"}
-              font={fonts.logo}
+        <Flex flexDirection="column" m={"1.625rem 4rem"}>
+          <Flex justifyContent="space-between">
+            <Box>
+              <Logo
+                color={colors.text}
+                open={false}
+                fontSize={"1.75rem"}
+                font={fonts.logo}
               >
                 Cooking
               </Logo>
-            <Box
-              mt={"0.75rem"}
-              color="#000"
-            >
-              Aprenda a cozinhar <Text display="inline" color={colors.subTitle}>de verdade</Text>.
-            </Box>
-            <Box mt={"1rem"}>
-              <Box width="10px">
-                <Bloob2/>
+              <Box mt={"0.75rem"} color="#000">
+                Aprenda a cozinhar{" "}
+                <Text display="inline" color={colors.subTitle}>
+                  de verdade
+                </Text>
+                .
               </Box>
-              <Box width="10px" ml="3rem">
-                <Bloob/>
+              <Box mt={"1rem"}>
+                <Box width="10px">
+                  <Bloob2 />
+                </Box>
+                <Box width="10px" ml="3rem">
+                  <Bloob />
+                </Box>
               </Box>
             </Box>
-          </Box>
-          <Flex
-            justifyContent="space-between"
-            ml={"2rem"}
-            mr={"4rem"}
-          >
-            <Box>
-              <Text color={colors.subTitle} fontWeight={"bold"}>Especiais</Text>
-              {especialRecipesPages.map((page) => (
-                <Box
-                  key={page.title}
-                  fontSize={"0.9rem"}
-                  pt={"1rem"}
-                  _hover={{ fontWeight: "bold", transition: 'all 600ms' }}
-                >
-                  <Link to={`/${page.page}`}>{page.title}</Link>
-                </Box>
-              ))}
-            </Box>
-            <Box ml={"8rem"}>
-              <Text color={colors.subTitle} fontWeight={"bold"}>Sobre</Text>
-              {aboutPages.map((page) => (
-                <Box
-                  key={page.title}
-                  fontSize={"0.9rem"}
-                  pt={"1rem"}
-                  _hover={{ fontWeight: "bold", transition: 'all 600ms' }}
-                >
-                  <Link to={`/${page.page}`}>{page.title}</Link>
-                </Box>
-              ))}
-            </Box>
+            <Flex justifyContent="space-between" ml={"2rem"} mr={"4rem"}>
+              <Box>
+                <Text color={colors.subTitle} fontWeight={"bold"}>
+                  Especiais
+                </Text>
+                {especialRecipesPages.map((page) => (
+                  <Box
+                    key={page.title}
+                    fontSize={"0.9rem"}
+                    pt={"1rem"}
+                    _hover={{ fontWeight: "bold", transition: "all 600ms" }}
+                  >
+                    <Link to={`/${page.page}`}>{page.title}</Link>
+                  </Box>
+                ))}
+              </Box>
+              <Box ml={"8rem"}>
+                <Text color={colors.subTitle} fontWeight={"bold"}>
+                  Sobre
+                </Text>
+                {aboutPages.map((page) => (
+                  <Box
+                    key={page.title}
+                    fontSize={"0.9rem"}
+                    pt={"1rem"}
+                    _hover={{ fontWeight: "bold", transition: "all 600ms" }}
+                  >
+                    <Link to={`/${page.page}`}>{page.title}</Link>
+                  </Box>
+                ))}
+              </Box>
+            </Flex>
           </Flex>
+          <Box pt="30px">
+            <Text fontSize={"0.75rem"}>
+              © Cooking™, 2021. All rights reserved.
+            </Text>
+          </Box>
         </Flex>
-        <Box pt="30px">
-          <Text fontSize={"0.75rem"}>© Cooking™, 2021. All rights reserved.</Text>
-        </Box>
-      </Flex>
       ) : (
         <Flex
           alignItems="center"
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,35 +1,41 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { ThemeContext } from 'styled-components'
-import { Head, Nav, NavLinks, StyledBurger, BloobBox } from './styles'
-import { SearchBar } from './SearchBar'
-import { Logo } from '../MainStyles'
-import { Bloob } from './Bloob'
-import useMedia from '../../hooks/useMedia'
-import bloob3 from '../../assets/Vetor Header 1.svg'
+import React, { useContext, useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { ThemeContext } from "styled-components"
+import { Head, Nav, NavLinks, StyledBurger, BloobBox } from "./styles"
+import { SearchBar } from "./SearchBar"
+import { Logo } from "../MainStyles"
+import { Bloob } from "./Bloob"
+import useMedia from "../../hooks/useMedia"
+import bloob3 from "../../assets/Vetor Header 1.svg"
 
 const Header = () => {
   const { fonts, colors } = useContext(ThemeContext)
   const [burguerOpen, setBurgerOpen] = useState(false)
-  const mobile = useMedia('(max-width: 48rem)')
-  const large = useMedia('(min-width: 62.5rem)')
+  const mobile = useMedia("(max-width: 48rem)")
+  const large = useMedia("(min-width: 62.5rem)")
 
   return (
     <Head mobile={mobile} url={bloob3} large={large}>
       <Nav mobile={mobile} scroll={scroll}>
         <Link to="/">
-          <Logo color={colors.text} mobile={mobile} font={fonts.logo}>Cooking</Logo>
+          <Logo color={colors.text} mobile={mobile} font={fonts.logo}>
+            Cooking
+          </Logo>
         </Link>
-        <NavLinks color={colors.text} hover={colors.subTitle} open={burguerOpen}>
+        <NavLinks
+          color={colors.text}
+          hover={colors.subTitle}
+          open={burguerOpen}
+        >
           <ul>
             <li>
               <Link to="/">Início</Link>
             </li>
             <li>
-              <Link to="/recipes">Receitas</Link>
+              <Link to="/receitas">Receitas</Link>
             </li>
             <li>
-              <Link to="/highlights">Destaques</Link>
+              <Link to="/destaques">Destaques</Link>
             </li>
             <li>
               <Link to="/chefs">Chefs</Link>
@@ -37,29 +43,27 @@ const Header = () => {
           </ul>
           {mobile && (
             <div>
-              <Link to="/about">QUEM SOMOS</Link>
-              <Link to="/about">CONTATO</Link>
-              <Link to="/about">TERMOS</Link>
+              <Link to="/sobre">QUEM SOMOS</Link>
+              <Link to="/sobre">CONTATO</Link>
+              <Link to="/sobre">TERMOS</Link>
             </div>
           )}
         </NavLinks>
         {mobile && (
-          <StyledBurger open={burguerOpen} onClick={() => setBurgerOpen(!burguerOpen)}>
+          <StyledBurger
+            open={burguerOpen}
+            onClick={() => setBurgerOpen(!burguerOpen)}
+          >
             <div />
             <div />
             <div />
           </StyledBurger>
         )}
 
-        {large && (
-          <SearchBar/>
-        )}
+        {large && <SearchBar />}
       </Nav>
 
-      {!mobile && (
-        <Bloob/>
-      )}
-
+      {!mobile && <Bloob />}
     </Head>
   )
 }
