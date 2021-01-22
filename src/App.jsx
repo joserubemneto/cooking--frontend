533eada02e3a5a2bb136c598164d63c402c163c0
Jose Rubem <joserubemn31@gmail.com>
2021-01-22 23:51:38 +0000

    refactor: added routes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,15 @@
 import React from "react"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 import { ChakraProvider } from "@chakra-ui/react"
 import { GlobalStyle } from "./components/MainStyles"
 import theme from "./theme"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import Landing from "./pages"
-import Recipes from "./pages/recipes"
-import Recipe from "./pages/recipe"
-import Highlights from "./pages/highlights"
-import About from "./pages/about"
-import Chefs from "./pages/chefs"
-import Chef from "./pages/chef"
-import Category from "./pages/category"
+import Routes from "./routes"
 
 const App = () => (
   <ChakraProvider>
     <ThemeProvider theme={theme}>
       <GlobalStyle font={theme.fonts.body} />
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route path="/sobre">
-            <About />
-          </Route>
-          <Route path="/receitas">
-            <Recipes />
-          </Route>
-          <Route path="/categoria/:id">
-            <Category />
-          </Route>
-          <Route path="/receita/:id">
-            <Recipe />
-          </Route>
-          <Route path="/destaques">
-            <Highlights />
-          </Route>
-          <Route path="/chefs">
-            <Chefs />
-          </Route>
-          <Route path="/chef/:id">
-            <Chef />
-          </Route>
-          <Route path="/chef/:id"></Route>
-        </Switch>
-        <Footer />
-      </BrowserRouter>
+      <Routes />
     </ThemeProvider>
   </ChakraProvider>
 )
---
+++ src/routes.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Landing from "./pages"
+import Recipes from "./pages/recipes"
+import Recipe from "./pages/recipe"
+import Highlights from "./pages/highlights"
+import About from "./pages/about"
+import Chefs from "./pages/chefs"
+import Chef from "./pages/chef"
+import Category from "./pages/category"
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route path="/sobre">
+          <About />
+        </Route>
+        <Route path="/receitas">
+          <Recipes />
+        </Route>
+        <Route path="/categoria/:id">
+          <Category />
+        </Route>
+        <Route path="/receita/:id">
+          <Recipe />
+        </Route>
+        <Route path="/destaques">
+          <Highlights />
+        </Route>
+        <Route path="/chefs">
+          <Chefs />
+        </Route>
+        <Route path="/chef/:id">
+          <Chef />
+        </Route>
+        <Route path="/chef/:id"></Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default Routes
