c12bd30d71f6c546255752ca2d3601e4a31d88f0
Jose Rubem <joserubemn31@gmail.com>
2021-01-29 01:34:48 +0000

    feat: created error component

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Flex, Text } from '@chakra-ui/react'
+
+const Error = () => {
+  return (
+    <Flex justify='center' align='center'>
+      <Text>Erro, tente novamente</Text>
+    </Flex>
+  )
+}
+
+export default Error
