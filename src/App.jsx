e7c2c5ecfe56dbc64bd65725cd612d51e4cb25f0
Jose Rubem <joserubemn31@gmail.com>
2021-01-29 01:34:22 +0000

    feat: added request context

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { ChakraProvider } from '@chakra-ui/react'
+import { RequestProvider } from './context/Request'
 import { GlobalStyle } from './components/MainStyles'
 import theme from './theme'
 import Routes from './routes'
@@ -9,7 +10,9 @@ const App = () => (
   <ChakraProvider>
     <ThemeProvider theme={theme}>
       <GlobalStyle font={theme.fonts.body} />
-      <Routes />
+      <RequestProvider>
+        <Routes />
+      </RequestProvider>
     </ThemeProvider>
   </ChakraProvider>
 )
