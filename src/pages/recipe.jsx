5f8ff107bef3b04c9d308269af2400e233642a43
Jose Rubem <joserubemn31@gmail.com>
2021-02-02 22:49:37 +0000

    fix: resolved data refresh error

--- src/pages/recipe.jsx
+++ src/pages/recipe.jsx
@@ -14,18 +14,16 @@ import Error from '../components/Error'
 
 const Recipe = () => {
   const [recipes, setRecipes] = useState([])
-  const [recipe, setRecipe] = useState([]);
+  const [recipe, setRecipe] = useState([])
   const { id } = useParams()
   const { loading, setLoading, error, setError } = useRequest()
-  console.log(recipes);
-  console.log(recipe);
 
   const requestData = useCallback(async () => {
     try {
       setLoading(true)
       const { data: recipesData } = await getRecipes()
-      setRecipes(recipesData)
       const { data: recipeData } = await getRecipe(id)
+      setRecipes(recipesData)
       setRecipe(recipeData)
       setLoading(false)
     } catch (error) {
@@ -36,102 +34,103 @@ const Recipe = () => {
 
   useEffect(() => {
     requestData()
-  }, [])
-
+  }, [id])
 
   return (
     <>
       {!loading && !error && (
-      <>
-      <Grid templateColumns='11% 51% 27% 11%' templateRows='repeat(6, auto)'>
-        <GridItem colStart={2} colSpan={2}>
-          <Flex>
-            <TopTitle color={theme.colors.title}>{recipe.title}</TopTitle>
-            <Spacer />
-            <Flex alignItems='center'>
-              <p>voltar para </p>
-              <p
-                style={{
-                  display: 'inline-block',
-                  marginLeft: '7px',
-                  color: `${theme.colors.subTitle}`,
-                }}>
-                {recipes.category_name}
-              </p>
-              <Link to='/recipes'>
-                <Image
-                  boxSize='35px'
-                  objectFit='cover'
-                  src={Back}
-                  alt='Dan Abramov'
-                  zIndex='-1'
-                  cursor='pointer'
-                  borderRadius='2rem'
-                  ml='10px'
-                />
-              </Link>
-            </Flex>
-          </Flex>
-        </GridItem>
-        <GridItem colSpan={2} colStart={2}>
-          <Image
-            mt='30px'
-            boxSize='75%'
-            height='400px'
-            objectFit='cover'
-            src={Photo}
-            alt='Dan Abramov'
-            zIndex='-1'
-            cursor='pointer'
-          />
-        </GridItem>
-        <GridItem colStart={2}>
-          <TopTitle Talign='left' color={theme.colors.subTitle}>
-            Resumo
-          </TopTitle>
-          <Text fontSize='18px' lineHeight='27px'>
-            resumo do resumo do resumo do resumo resumo do resumo do resumo do
-            resumoresumo do resumo do resumo do resumoresumo do resumo do resumo
-            do resumoresumo do resumo do resumo do resumoresumo do resumo do
-            resumo do resumoresumo do resumo do resumo do resumoresumo do resumo
-            do resumo do resumo
-          </Text>
-        </GridItem>
-        <GridItem colStart={2} mt='20px'>
-          <TopTitle Talign='left' color={theme.colors.subTitle}>
-            Ingredientes
-          </TopTitle>
-          <div>
-            {recipe.ingredients.map((ingredient) => (
-              <Flex key={ingredient.id} align='center' mt='5px'>
-                <Image src={Elipse} alt={'Elipse'} p='0 1rem' />
-                <Text fontWeight='medium' fontSize='1.3rem'>
-                  {ingredient}
-                </Text>
+        <>
+          <Grid
+            templateColumns='11% 51% 27% 11%'
+            templateRows='repeat(6, auto)'>
+            <GridItem colStart={2} colSpan={2}>
+              <Flex>
+                <TopTitle color={theme.colors.title}>{recipe.title}</TopTitle>
+                <Spacer />
+                <Flex alignItems='center'>
+                  <p>voltar para </p>
+                  <p
+                    style={{
+                      display: 'inline-block',
+                      marginLeft: '7px',
+                      color: `${theme.colors.subTitle}`,
+                    }}>
+                    {recipes.category_name}
+                  </p>
+                  <Link to='/recipes'>
+                    <Image
+                      boxSize='35px'
+                      objectFit='cover'
+                      src={Back}
+                      alt='Dan Abramov'
+                      zIndex='-1'
+                      cursor='pointer'
+                      borderRadius='2rem'
+                      ml='10px'
+                    />
+                  </Link>
+                </Flex>
               </Flex>
-            ))}
-          </div>
-        </GridItem>
-        <GridItem colStart={2}>
-          <TopTitle Talign='left' color={theme.colors.subTitle}>
-            Modo de Preparo
-          </TopTitle>
-          <div>
-            {recipe.preparation.map((preparation, index) => (
-              <Flex key={preparation.id} align='center' mt='20px' ml='15px'>
-                <Text fontSize='1.7rem' color={theme.colors.title}>
-                  {index + 1}.
-                </Text>
-                <Text fontSize='1.3rem' ml='20px' fontWeight='light'>
-                  {preparation}
-                </Text>
-              </Flex>
-            ))}
-          </div>
-        </GridItem>
-      </Grid>
-      <CarouselOne title='Outras Receitas' data={recipes} />
-    </>
+            </GridItem>
+            <GridItem colSpan={2} colStart={2}>
+              <Image
+                mt='30px'
+                boxSize='75%'
+                height='400px'
+                objectFit='cover'
+                src={Photo}
+                alt='Dan Abramov'
+                zIndex='-1'
+                cursor='pointer'
+              />
+            </GridItem>
+            <GridItem colStart={2}>
+              <TopTitle Talign='left' color={theme.colors.subTitle}>
+                Resumo
+              </TopTitle>
+              <Text fontSize='18px' lineHeight='27px'>
+                resumo do resumo do resumo do resumo resumo do resumo do resumo
+                do resumoresumo do resumo do resumo do resumoresumo do resumo do
+                resumo do resumoresumo do resumo do resumo do resumoresumo do
+                resumo do resumo do resumoresumo do resumo do resumo do
+                resumoresumo do resumo do resumo do resumo
+              </Text>
+            </GridItem>
+            <GridItem colStart={2} mt='20px'>
+              <TopTitle Talign='left' color={theme.colors.subTitle}>
+                Ingredientes
+              </TopTitle>
+              <div>
+                {recipe.ingredients.map((ingredient) => (
+                  <Flex key={ingredient.id} align='center' mt='5px'>
+                    <Image src={Elipse} alt={'Elipse'} p='0 1rem' />
+                    <Text fontWeight='medium' fontSize='1.3rem'>
+                      {ingredient}
+                    </Text>
+                  </Flex>
+                ))}
+              </div>
+            </GridItem>
+            <GridItem colStart={2}>
+              <TopTitle Talign='left' color={theme.colors.subTitle}>
+                Modo de Preparo
+              </TopTitle>
+              <div>
+                {recipe.preparation.map((preparation, index) => (
+                  <Flex key={preparation.id} align='center' mt='20px' ml='15px'>
+                    <Text fontSize='1.7rem' color={theme.colors.title}>
+                      {index + 1}.
+                    </Text>
+                    <Text fontSize='1.3rem' ml='20px' fontWeight='light'>
+                      {preparation}
+                    </Text>
+                  </Flex>
+                ))}
+              </div>
+            </GridItem>
+          </Grid>
+          <CarouselOne title='Outras Receitas' data={recipes} />
+        </>
       )}
       {loading && !error && <Loading />}
       {error && <Error />}
