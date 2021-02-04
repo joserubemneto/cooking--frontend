b683505c2265bd6a1952b751591ee88e9ea84250
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-02-03 21:09:01 -0300

    feat(integration): get categories done

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -1,76 +1,108 @@
-import React from 'react'
+import React, { useEffect, useCallback, useState } from 'react'
 import { Grid, GridItem, Image, Flex, Spacer } from '@chakra-ui/react'
 import { TopTitle } from '../components/MainStyles'
 import theme from '../theme'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Back from '../assets/back.svg'
 import { Box } from '../components/CarouselOne/styles'
 import Imagem from '../assets/Image1.jpg'
 import CategoryCard from '../components/CategoryCard'
+import { useRequest } from '../context/Request'
+import { getCategory } from '../services/api'
+import Loading from '../components/Loading'
+import Error from '../components/Error'
 
 const Category = () => {
+  const [categories, setCategories] = useState([])
+  const { id } = useParams()
+  const { loading, setLoading, error, setError } = useRequest()
+
+  const requestData = useCallback(async () => {
+    try {
+      setLoading(true)
+      const { data: categoriesData } = await getCategory(id)
+      setCategories(categoriesData)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setError(true)
+    }
+  })
+
+  useEffect(() => {
+    requestData()
+  }, [])
+
   return (
     <>
-      <Grid templateColumns='11% 39% 39% 11%' templateRows='repeat(6, auto)'>
-        <GridItem colStart={2} colSpan={2}>
-          <Flex>
-            <TopTitle color={theme.colors.title}>Bebidas</TopTitle>
-            <Spacer />
-            <Flex alignItems='center'>
-              <p>voltar para </p>
-              <p
-                style={{
-                  display: 'inline-block',
-                  marginLeft: '5px',
-                  color: `${theme.colors.subTitle}`,
-                }}>
-                Receitas
-              </p>
-              <Link to='/recipes'>
+      {!loading && !error && (
+        <>
+          <Grid
+            templateColumns='11% 39% 39% 11%'
+            templateRows='repeat(6, auto)'>
+            <GridItem colStart={2} colSpan={2}>
+              <Flex>
+                <TopTitle color={theme.colors.title}>Bebidas</TopTitle>
+                <Spacer />
+                <Flex alignItems='center'>
+                  <p>voltar para </p>
+                  <Link to='/receitas'>
+                    <p
+                      style={{
+                        display: 'inline-block',
+                        marginLeft: '5px',
+                        color: `${theme.colors.subTitle}`,
+                      }}>
+                      Receitas
+                    </p>
+                    <Image
+                      boxSize='35px'
+                      objectFit='cover'
+                      src={Back}
+                      alt='Voltar'
+                      zIndex='-1'
+                      cursor='pointer'
+                      borderRadius='2rem'
+                      ml='10px'
+                    />
+                  </Link>
+                </Flex>
+              </Flex>
+            </GridItem>
+            <GridItem colStart={2} colSpan={2} mt='35px' marginBottom='30px'>
+              <Box bg='transparent' mobile='true'>
                 <Image
-                  boxSize='35px'
+                  boxSize='100%'
                   objectFit='cover'
-                  src={Back}
-                  alt='Voltar'
+                  src={Imagem}
+                  alt='Dan Abramov'
+                  position='absolute'
                   zIndex='-1'
-                  cursor='pointer'
                   borderRadius='2rem'
-                  ml='10px'
                 />
-              </Link>
-            </Flex>
-          </Flex>
-        </GridItem>
-        <GridItem colStart={2} colSpan={2} mt='35px' marginBottom='30px'>
-          <Box bg='transparent' mobile='true'>
-            <Image
-              boxSize='100%'
-              objectFit='cover'
-              src={Imagem}
-              alt='Dan Abramov'
-              position='absolute'
-              zIndex='-1'
-              borderRadius='2rem'
-            />
-          </Box>
-        </GridItem>
-        <GridItem colStart={2} colSpan={2}>
-          <Grid templateColumns='1fr 1fr'>
-            <GridItem margin=' 0 30px'>
-              <CategoryCard Name='lucas melo' />
+              </Box>
             </GridItem>
-            <GridItem margin='0 30px'>
-              <CategoryCard Name='lucas melo' />
-            </GridItem>
-            <GridItem margin='0 30px'>
-              <CategoryCard Name='lucas melo' />
-            </GridItem>
-            <GridItem margin='0 30px'>
-              <CategoryCard Name='lucas melo' />
+            <GridItem colStart={2} colSpan={2}>
+              <Grid templateColumns='1fr 1fr'>
+                <GridItem margin=' 0 30px'>
+                  <CategoryCard Name='lucas melo' />
+                </GridItem>
+                <GridItem margin='0 30px'>
+                  <CategoryCard Name='lucas melo' />
+                </GridItem>
+                <GridItem margin='0 30px'>
+                  <CategoryCard Name='lucas melo' />
+                </GridItem>
+                <GridItem margin='0 30px'>
+                  <CategoryCard Name='lucas melo' />
+                </GridItem>
+              </Grid>
             </GridItem>
           </Grid>
-        </GridItem>
-      </Grid>
+        </>
+      )}
+      {loading && <Loading />}
+      {error && <Error />}
     </>
   )
 }
