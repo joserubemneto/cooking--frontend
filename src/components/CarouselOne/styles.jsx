e064064cc200e0b3d6603e3117623ce01d95d917
Jose Rubem <joserubemn31@gmail.com>
2021-02-06 16:21:49 +0000

    fix: adjust carousel one width

--- src/components/CarouselOne/styles.jsx
+++ src/components/CarouselOne/styles.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const Box = styled.div`
-  width: ${({ mobile }) => (mobile ? '100%' : '28vw')};
+  width: ${({ mobile }) => (mobile ? '100%' : 'auto')};
   height: 290px;
 
   position: relative;
@@ -11,6 +11,8 @@ const Box = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  margin-left: 1.25rem;
 
   border-radius: 2rem;
 `
