0bb10f75ca8cdb82312b938c6ec14bb585af48af
Jose Rubem <joserubemn31@gmail.com>
2021-02-06 16:05:49 +0000

    fix: resolve arrows bug

--- src/components/CarouselTwo/Slider.jsx
+++ src/components/CarouselTwo/Slider.jsx
@@ -1,75 +1,65 @@
 import React from 'react'
 import Slider from 'react-slick'
-import { Box, UnorderedList, css, Button } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import SingleSlide from './SingleSlide'
 import '../CarouselOne/Slider.css'
-import ArrowLeft from '../../assets/Arrow Left.svg'
-import ArrowRight from '../../assets/Arrow Right.svg'
 
 const SampleNextArrow = (props) => {
-  const { onClick } = props
+  const { className, style, onClick } = props
   return (
-    <Box
-      display='block'
-      bg='transparent'
-      w='40px'
-      h='20px'
-      position='absolute'
-      top='18.2rem'
-      left='29%'
-      bgImage={`url("${ArrowRight}")`}
-      onClick={onClick}
-      bgSize='cover'
-      m='0'
-      p='0'
-      zIndex='2'
-      cursor='pointer'
-      _before={{
-        content: `""`,
+    <div
+      id='oneNext'
+      className={className}
+      style={{
+        ...style,
+        display: 'block',
+        position: 'relative',
+        top: '47px',
+        right: '-63%',
+        padding: '0',
+        margin: '0',
+        zIndex: '2',
       }}
+      onClick={onClick}
     />
   )
 }
 
 const SamplePrevArrow = (props) => {
-  const { onClick } = props
+  const { className, style, onClick } = props
   return (
-    <Box
-      display='block'
-      bg='transparent'
-      w='40px'
-      h='20px'
-      position='absolute'
-      top='18.2em'
-      left='25%'
-      bgImage={`url("${ArrowLeft}")`}
-      onClick={onClick}
-      bgSize='cover'
-      zIndex='2'
-      m='0'
-      p='0'
-      cursor='pointer'
-      _before={{
-        content: `""`,
+    <div
+      id='onePrev'
+      className={className}
+      style={{
+        ...style,
+        display: 'block',
+        position: 'relative',
+        top: '308px',
+        left: '33%',
+        padding: '0',
+        margin: '0',
+        zIndex: '2',
       }}
+      onClick={onClick}
     />
   )
 }
 
-const CenterMode = ({ data }) => {
+const MultipleItems = ({ data }) => {
   const settings = {
     dots: true,
     appendDots: (dots) => (
-      <Box
-        display='flex'
-        position='absolute'
-        top='18.2rem'
-        bgColor='transparent'
-        p='0'
-        m='-5px'
-        ml='5rem'>
-        <UnorderedList id='oneDot'>{dots}</UnorderedList>
-      </Box>
+      <div
+        style={{
+          backgroundColor: 'transparent',
+          padding: '0',
+        }}>
+        <ul id='oneDot' style={{ margin: '-10px' }}>
+          {' '}
+          {dots}{' '}
+        </ul>
+      </div>
     ),
     className: 'center',
     centerMode: true,
@@ -91,4 +81,4 @@ const CenterMode = ({ data }) => {
   )
 }
 
-export default CenterMode
+export default MultipleItems
