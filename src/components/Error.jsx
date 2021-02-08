e7fba4853f7b5ad42010fd3a96491b012a25ad4a
Jose Rubem <joserubemn31@gmail.com>
2021-02-08 00:54:08 +0000

    feat: added link and redirect

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Text, Link, Box } from '@chakra-ui/react'
+import { Link as ReactLink } from 'react-router-dom'
+import { useRequest } from '../context/Request'
 
 const Error = () => {
+  const { setError } = useRequest()
   return (
-    <Flex justify='center' align='center'>
-      <Text>Erro, tente novamente</Text>
+    <Flex justify='center' align='center' direction='column'>
+      <Box>
+        Algo correu mal,
+        <Link
+          color='#D64D27'
+          as={ReactLink}
+          to='/'
+          onClick={() => {
+            setError(false)
+          }}>
+          {' '}
+          clique aqui!
+        </Link>
+      </Box>
+      <Text mt='1rem'>
+        Caso o erro persista pedimos sua compreensão e vamos resolver em breve.
+      </Text>
     </Flex>
   )
 }
